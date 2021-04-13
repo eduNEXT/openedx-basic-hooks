8a2a45a833f51084666b4eaf9b935a75d4fad50e
Luis Moreno <morenol@users.noreply.github.com>
2021-04-13 13:24:50 -0400

    Tweet registration

--- openedx_basic_hooks/receivers.py
+++ openedx_basic_hooks/receivers.py
@@ -1,10 +1,12 @@
 """
-Receivers for the openedx-events signals
+Receivers for the openedx-events signals.
 """
 
+from openedx_basic_hooks.actions.post_register import post_register_tweet
 
-def registration_receiver(*args, **kwargs):
+
+def registration_receiver(user, registration, *args, **kwargs):
     """
-    Registration receiver
+    Registration receiver.
     """
-    pass
+    post_register_tweet(user, registration, *args, **kwargs)
